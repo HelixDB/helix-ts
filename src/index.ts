bda76020e8db28d4c56de177b6d6f1512b5357a0
Gahn Wuwong <138665743+Gahnxd@users.noreply.github.com>
2025-09-21 10:55:41 -0700

    add api keys

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export interface HelixDBClient {
   url: string;
+  apiKey: string | null;
   /**
    * Query the HelixDB API
    * @param endpoint - The endpoint to query
@@ -14,15 +15,15 @@ export type HelixDBInput = Record<string, any>;
 
 class HelixDB implements HelixDBClient {
   url: string;
-
+  apiKey: string | null;
   /**
    * Create a new HelixDB client
-   * @param url - The url of the HelixDB server
-   *
-   * (default: `http://localhost:6969`)
+   * @param url - The url of the HelixDB server (default: `http://localhost:6969`)
+   * @param apiKey - The api key of the HelixDB server (default: `null`)
    */
-  constructor(url: string = "http://localhost:6969") {
+  constructor(url: string = "http://localhost:6969", apiKey: string | null = null) {
     this.url = url;
+    this.apiKey = apiKey;
   }
 
   async query(endpoint: string, data: HelixDBInput): Promise<HelixDBResponse> {
@@ -30,6 +31,7 @@ class HelixDB implements HelixDBClient {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        ...(this.apiKey ? { "x-api-key": this.apiKey } : {}),
       },
       body: JSON.stringify(data),
     });
